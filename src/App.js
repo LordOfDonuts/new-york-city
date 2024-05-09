fab03c4d7f5f69964ded163cdf3c6f746fadb021
Ivan <my_lord@macbook.local>
2024-05-08 21:08:44 -0700

    All responsive

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import FiveBoroughs from './sections/five-boroughs/FiveBoroughs';
 import Timeline from './sections/timeline/Timeline';
 import PlacesToVisit from './sections/places-to-visit/PlacesToVisit';
 
+import SectionTransition from './components/SectionTransition';
+
 function App() {
   return (
     <div>
       <StartScreen />
+      <SectionTransition />
       <Timeline />
+      <SectionTransition />
       <PlacesToVisit />
+      <SectionTransition />
       <FiveBoroughs />
+      <SectionTransition />
       <FunFacts />
     </div>
   );
---
+++ src/components/SectionTransition.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionTransition = () => {
+  return (
+    <div className='section-transition'></div>
+  )
+}
+
+export default SectionTransition
